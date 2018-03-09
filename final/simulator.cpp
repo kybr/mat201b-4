16440e0e4d361986d83b1a7b39d29f3997bb0e46
ben <benedic.lee@gmail.com>
2018-03-09 08:54:42 -0800

    ben's final

--- final/simulator.cpp
+++ final/simulator.cpp
@@ -26,6 +26,7 @@
 #include "Gamma/Oscillator.h"
 #include "allocore/math/al_Ray.hpp"
 #include "allocore/math/al_Vec.hpp"
+#include "common.hpp"
 
 using namespace al;
 using namespace std;
@@ -36,18 +37,17 @@ float dragFactor = 0.1;           //
 float timeStep = 0.1;        // keys change this value for effect
 float scaleFactor = 0.1;          // resizes the entire scene
 // Planet const.
-unsigned planetCount = 23;
-float planetRadius = 10;  // increase this to make collisions more frequent
+int planetCount = 23;
+float planetRadius = 20;  // 
 float planetRange = 500;
 // Comet const.
 float cometRadius = 20;
 float steerFactor = 100;
 // Dust const.
 unsigned dustCount = 300;       
-float dustRange = 1000;
+float dustRange = 1500;
 float dustRadius1 = 0.5;
 float dustRadius2 = 1;
-
 bool keys[4];
 Mesh comet; 
 Mesh planet;
@@ -59,23 +59,26 @@ Vec3d r() { return Vec3f(rnd::uniformS(), rnd::uniformS(), rnd::uniformS()); }
 
 struct Comet {
   Vec3f position,velocity,acceleration;
-  float abs_speed, unit_rotate, force, head, head_angv;
+  float abs_speed, unit_rotate, force, head, head_angv, vel_angle;
   Color ton;
   Mesh comet;
   Texture texture;
   Image image; 
   Comet() {
-    position = Vec3d(0,0,0);
+    position = Vec3f(0,0,0);
+    velocity = Vec3f(0,0,0);
     head = 0;
     head_angv = 0;
     ton = HSV(0.5, 0.2, 1);
     unit_rotate = 0.02;
-    force = 0.1;
+    force = 0.01;
     addSphereWithTexcoords(comet);
     SearchPaths searchPaths;
-//    searchPaths.addSearchPath("../media");
-    searchPaths.addSearchPath("/home/ben/Desktop/work/AlloSystem/mat201b/ben");
-    string filename = searchPaths.find("test.png").filepath();
+    searchPaths.addSearchPath("");
+//    searchPaths.addSearchPath("/home/ben/Desktop/work/AlloSystem/mat201b/media");
+//    searchPaths.addSearchPath("/home/ben/Desktop/work/AlloSystem/mat201b/ben/final");
+
+    string filename = searchPaths.find("ben_comet.jpg").filepath();
     if (image.load(filename)) {
       cout << "Read image from " << filename << endl;
     } else {
@@ -95,12 +98,12 @@ struct Comet {
     g.popMatrix();
     head_angv *= 0.95;
     head += head_angv * timeStep;
-    /*if (head > 2*M_PI) {
+    if (head > M_PI) {
       head -= 2*M_PI;
     }
-    if (head < 0) {
+    if (head < -M_PI) {
       head += 2*M_PI;
-    }*/
+    }
   }
 };
 
@@ -154,20 +157,29 @@ struct Stardust2 {
  }
 };
 
-struct MyApp : App {
+struct MyApp : App, osc::PacketHandler {
   Material material;
   Light light;
+  Pose pose;
+  Vec3f cell_vel = Vec3f(0,0,0);
   Vec3f camera_position = Vec3f(c.position.x - 10, c.position.y - 10,  c.position.z + 10);
+
+  Vec3f diff_to_comet[23];
+  float distance_to_comet[23];
 
   bool simulate = true;
   unsigned frameCount = 0;
   Planet p;
   Comet c;
+  
   Stardust1 d1;
   Stardust2 d2;
   vector<Planet> planets;
   vector<Stardust1> dusts1;
   vector<Stardust2> dusts2;
+  // Gamma
+  gam::SineD<> sined;
+  gam::Accum<> timer;
 
   MyApp() {
     Pose pose;
@@ -177,8 +189,6 @@ struct MyApp : App {
     addSphere(planet, planetRadius);
     addSphere(dust1, dustRadius1);
     addSphere(dust1, dustRadius2);
-//		add(new StandardWindowKeyControls);
-//		add(new NavInputControl(nav));
 
     planet.generateNormals();
     dust1.generateNormals();
@@ -186,10 +196,11 @@ struct MyApp : App {
 
 
 //    light.pos(0, 0, 10);              // place the light
-    nav.pos(0, 0, -100);             // place the viewer
+    nav.pos(0, 0, -100);             // place the init. viewer
     lens().far(400);                 // set the far clipping plane
     planets.resize(planetCount);  // make all the planets
-    dusts1.resize(dustCount);  // make all the planets
+    dusts1.resize(dustCount);  // make all the dusts1
+    dusts2.resize(dustCount);  // make all the dusts2
 
     background(Color(0.07));
 
@@ -200,6 +211,9 @@ struct MyApp : App {
 
     initWindow();
     initAudio();
+    oscRecv().open(60777,"",0.016, Socket::UDP);
+    oscRecv().handler(*this);
+    oscRecv().start();
   }
 
   void onAnimate(double dt) {	
@@ -218,17 +232,28 @@ struct MyApp : App {
     c.acceleration += c.velocity * -dragFactor;
     c.position += c.velocity * timeStep;
     c.abs_speed = sqrt( c.velocity[0] * c.velocity[0] + c.velocity[2] * c.velocity[2]);
-    camera_position[0] = c.position.x - ( cos(c.head) *10 ) ;
+
+    c.vel_angle = atan(c.velocity[0] / c.velocity[2]);
+    camera_position[0] = c.position.x + ( cos(c.head) *100 ) ;
     camera_position[1] = c.position.y ;
-    camera_position[2] = c.position.z - ( sin(c.head) *10 );
-    cout << c.position << "," << camera_position <<"," << c.head << endl;
+    camera_position[2] = c.position.z + ( sin(c.head) *100 );
+  //  cout << c.position << "," << camera_position <<"," << c.head << "," << c.vel_angle << endl;
+
+  // Distance btw comet and planets
+    for(int i=0 ; i < planetCount; i++){
+      diff_to_comet[i] = c.position - p.position[i];
+      distance_to_comet[i] = diff_to_comet[i].x * diff_to_comet[i].x + diff_to_comet[i].y*diff_to_comet[i].y + diff_to_comet[i].z*diff_to_comet[i].z; 
+    }
+
+
+
 
     g.pushMatrix();
-    nav().faceToward(c.position , 1);             // place the viewer
     nav().pos(camera_position);  // Face toward the comet
+    nav().faceToward(c.position , 1);             // place the viewer
+
     light.pos(c.position);
     g.popMatrix();
-
 
     frameCount++;
   }
@@ -247,9 +272,17 @@ struct MyApp : App {
   }
 
   void onSound(AudioIO& io) {
+    gam::Sync::master().spu(audioIO().fps());
     while (io()) {
-      io.out(0) = 0;
-      io.out(1) = 0;
+      if (timer()) {
+        // sined.set(rnd::uniform(220.0f, 880.0f), 0.5f, 1.0f);
+        for(int i=0 ; i < planetCount; i++){
+          sined.set(500.0f - distance_to_comet[i] * 6, 0.5f, 1.0f);
+        }
+      }
+      float s = sined();
+      io.out(0) = s;
+      io.out(1) = s;
     }
   }
 
@@ -266,7 +299,23 @@ struct MyApp : App {
     if (keys[3]) c.head_angv += c.unit_rotate;
  //   cout << c.head << endl; 
   }
+  void onMessage(osc::Message& m) {
+    Vec3f o;
+    if (m.addressPattern() == "/gyrosc/accel") {
+      m >> o.x;
+      m >> o.y;
+      m >> o.z;
+//      pose.pos(o);
+    } else
+//    m.print();
+    cell_vel.x += o.x;
+    cell_vel.y += o.y;
+    cell_vel.z += o.z;
+    cout << "Velo" << cell_vel << "\n"<< "Accel" << o << endl;
+//    cout << "Accel" << o << endl;
 
+  }
+  
 
   void onKeyDown(const ViewpointWindow&, const Keyboard& k) {
     switch (k.key()) {
